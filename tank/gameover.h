abe7ab41ac76248738464939cece4faa8e7a6ba9
hzx150 <2167753545@qq.com>
2024-07-11 16:48:29 +0800

    Update gameover.h

--- tank/gameover.h
+++ tank/gameover.h
@@ -12,12 +12,15 @@ class GameOver : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit GameOver(QWidget *parent = 0);
-    ~GameOver();
+    explicit GameOver(QWidget *parent = 0); // 显示构造函数声明
+    ~GameOver(); // 显示析构函数声明
+
 signals:
-    void retry();
+    void retry(); // retry信号声明
+
 private:
-    Ui::GameOver *ui;
+    Ui::GameOver *ui; // UI界面指针声明
 };
 
 #endif // GAMEOVER_H
+
