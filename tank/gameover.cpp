ef1d78a19fafc43cf3ccd78bfd4096d406770023
hzx150 <2167753545@qq.com>
2024-07-11 16:49:05 +0800

    Update gameover.cpp

--- tank/gameover.cpp
+++ tank/gameover.cpp
@@ -5,13 +5,13 @@ GameOver::GameOver(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::GameOver)
 {
-    ui->setupUi(this);
-    connect(ui->ReTty,&QPushButton::clicked,[=](){
-        emit this->retry();
+    ui->setupUi(this); // 设置UI界面
+    connect(ui->ReTty, &QPushButton::clicked, [=](){ // 连接点击按钮的信号和槽
+        emit this->retry(); // 发送retry信号
     });
 }
 
 GameOver::~GameOver()
 {
-    delete ui;
+    delete ui; // 删除UI界面指针
 }
