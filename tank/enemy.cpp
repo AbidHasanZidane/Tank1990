4a527c8d0d00e235956eda7e3350dc64b34e4692
hzx150 <2167753545@qq.com>
2024-07-11 16:41:05 +0800

    Update enemy.cpp

--- tank/enemy.cpp
+++ tank/enemy.cpp
@@ -2,9 +2,7 @@
 
 Enemy::Enemy(int x,int y)
 {
-    this->setPixmap(QPixmap("://90Tank/enemy/animation_sprite_126.png"));
+    this->setPixmap(QPixmap("://90Tank/enemy/animation_sprite_126.png"));  // 设置敌人的图片
 
-    this->setPos(x,y);
-
+    this->setPos(x,y);  // 设置敌人的初始位置
 }
-
