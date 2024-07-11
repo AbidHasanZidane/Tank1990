d04382f8cc902530fbe8f27b00b9d8e29767eca7
hzx150 <2167753545@qq.com>
2024-07-11 16:01:02 +0800

    Update gameover.cpp

--- tank/gameover.cpp
+++ tank/gameover.cpp
@@ -3,12 +3,12 @@
 
 GameOver::GameOver(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::GameOver)
+    ui(new Ui::GameOver) // 在堆上创建一个新的 Ui::GameOver 对象
 {
-    ui->setupUi(this);
+    ui->setupUi(this); // 调用 Ui::GameOver 类的 setupUi 方法来设置当前窗口
 }
 
-GameOver::~GameOver()
+GameOver::~GameOver() // 析构函数的定义
 {
-    delete ui;
+    delete ui; // 删除在堆上创建的 Ui::GameOver 对象，释放内存
 }
