827a8d01bd96ad780565d37f5ca64de6d5286bfc
hzx150 <2167753545@qq.com>
2024-07-11 16:00:11 +0800

    Update gameover.h

--- tank/gameover.h
+++ tank/gameover.h
@@ -4,7 +4,7 @@
 #include <QMainWindow>
 
 namespace Ui {
-class GameOver;
+class GameOver; // 声明命名空间 Ui 中的 GameOver 类
 }
 
 class GameOver : public QMainWindow
@@ -12,11 +12,11 @@ class GameOver : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit GameOver(QWidget *parent = 0);
-    ~GameOver();
+    explicit GameOver(QWidget *parent = 0); // 构造函数声明，parent 参数默认为 0
+    ~GameOver(); // 析构函数声明
 
 private:
-    Ui::GameOver *ui;
+    Ui::GameOver *ui; // Ui 命名空间中的 GameOver 类的指针
 };
 
 #endif // GAMEOVER_H
