77e0eccaca8ecdf68f36062f222e243ab2f5144a
AbidHasanZidane <abidhasanzidane@gmail.com>
2024-07-13 16:51:50 +0800

    lots of stupid build files
    No key files have been changed

---
+++ Tank Game v0.1.0/score.h
@@ -0,0 +1,14 @@
+#ifndef SCORE_H
+#define SCORE_H
+#include <QGraphicsTextItem>
+class Score: public QGraphicsTextItem
+{
+public:
+    Score(QGraphicsItem * parent = 0);
+    void increaseScore();
+    int getScore();
+private:
+    int score;
+};
+
+#endif // SCORE_H
