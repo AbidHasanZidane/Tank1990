842b97f2cd1f0e9408d0a56372d7d8b7f592ec65
hzx150 <2167753545@qq.com>
2024-07-16 19:17:55 +0800

    Update achievement.h

--- tank/achievement.h
+++ tank/achievement.h
@@ -3,7 +3,7 @@
 
 #include <QWidget>
 #include <QSoundEffect>
-#include "settings.h"
+#include "settings.h"  // 包含自定义设置的头文件
 
 namespace Ui {
 class achievement;
@@ -14,20 +14,22 @@ class achievement : public QWidget
     Q_OBJECT
 
 public:
-    explicit achievement(QWidget *parent = nullptr);
-    ~achievement();
-    QSoundEffect *butEffect=new QSoundEffect;
-    Ui::achievement *ui;
+    explicit achievement(QWidget *parent = nullptr);  // 构造函数
+    ~achievement();  // 析构函数
+
+    QSoundEffect *butEffect = new QSoundEffect;  // 声音效果对象
+
+    Ui::achievement *ui;  // UI对象指针
 
 signals:
-    void switch2w();
+    void switch2w();  // 切换到另一个窗口的信号
 
 private slots:
-    void on_pushButton_clicked();
+    void on_pushButton_clicked();  // 按钮点击的槽函数
 
 private:
-    void keyPressEvent(QKeyEvent *event) override;
-    bool eventFilter(QObject *obj, QEvent *event) override;
+    void keyPressEvent(QKeyEvent *event) override;  // 重写键盘按下事件处理函数
+    bool eventFilter(QObject *obj, QEvent *event) override;  // 重写事件过滤器函数
 };
 
 #endif // ACHIEVEMENT_H
