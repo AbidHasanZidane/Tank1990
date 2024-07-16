54a4a8e54c0d04078473f88c986685d40779359f
hzx150 <2167753545@qq.com>
2024-07-16 19:17:03 +0800

    Update Heart.h

--- tank/Heart.h
+++ tank/Heart.h
@@ -1,13 +1,16 @@
 #ifndef HEART_H
 #define HEART_H
-#include <QGraphicsPixmapItem>
-#include <QGraphicsItem>
-#include<QObject>
 
-class Heart: public QGraphicsPixmapItem,public QObject
+#include <QGraphicsPixmapItem> 
+#include <QGraphicsItem>     
+#include <QObject>        
+
+// 定义Heart类，继承自QGraphicsPixmapItem和QObject
+class Heart: public QGraphicsPixmapItem, public QObject
 {
 public:
-    Heart(int x,int y);
+    // 构造函数，接受x和y坐标参数，用于初始化Heart对象
+    Heart(int x, int y);
 };
 
 #endif // HEART_H
