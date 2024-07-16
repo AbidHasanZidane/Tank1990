7b8b1efe7730e870788f201a771796b68f60f345
hzx150 <2167753545@qq.com>
2024-07-16 19:23:49 +0800

    Update enemybullet.h

--- tank/enemybullet.h
+++ tank/enemybullet.h
@@ -1,21 +1,21 @@
 #ifndef ENEMYBULLET_H
 #define ENEMYBULLET_H
 
-#include <QGraphicsPixmapItem>
-#include <QGraphicsItem>
-#include<QtDebug>
-#include<QObject>
+#include <QGraphicsPixmapItem>  // 包含图形项类的头文件
+#include <QGraphicsItem>  // 包含图形项类的头文件
+#include <QtDebug>  // 包含Qt调试类的头文件
+#include <QObject>  // 包含QObject类的头文件，以便继承
 
-class enemyBullet : public QGraphicsPixmapItem,public QObject
+class enemyBullet : public QGraphicsPixmapItem, public QObject
 {
 public:
-    char _dir;
+    char _dir;  // 子弹移动方向
 
-    enemyBullet(QPoint _pos,QPixmap _pixmap,char _dir);
+    enemyBullet(QPoint _pos, QPixmap _pixmap, char _dir);  // 构造函数声明
 
-    int eBulletSpeed=40;
+    int eBulletSpeed = 40;  // 子弹移动速度
 
-    void eBulletMove();
+    void eBulletMove();  // 子弹移动函数声明
 };
 
 #endif // ENEMYBULLET_H
