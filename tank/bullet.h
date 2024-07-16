3fbdd6516028bc754c01a425ca4d96b2ede62dcb
hzx150 <2167753545@qq.com>
2024-07-16 19:22:09 +0800

    Update bullet.h

--- tank/bullet.h
+++ tank/bullet.h
@@ -3,19 +3,19 @@
 
 #include <QGraphicsPixmapItem>
 #include <QGraphicsItem>
-#include<QtDebug>
-#include<QObject>
+#include <QtDebug>
+#include <QObject>
 
-class Bullet : public QGraphicsPixmapItem,public QObject
+class Bullet : public QGraphicsPixmapItem, public QObject
 {
 public:
-    char _dir;
+    char _dir;  // 子弹的移动方向 ('N', 'S', 'W', 'E')
 
-    Bullet(QPoint _pos,QPixmap _pixmap,char _dir);
-    
-    int mBulletSpeed=40;
+    Bullet(QPoint _pos, QPixmap _pixmap, char _dir);  // 构造函数，初始化子弹的位置、图像和方向
 
-    void BulletMove();
-    
+    int mBulletSpeed = 40;  // 子弹的移动速度
+
+    void BulletMove();  // 子弹移动的方法
+
 };
 #endif // BULLET_H
