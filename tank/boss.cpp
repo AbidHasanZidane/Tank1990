779c2c98a62e196d343c8b3c36c9579a26d0d80e
hzx150 <2167753545@qq.com>
2024-07-16 19:19:57 +0800

    Update boss.cpp

--- tank/boss.cpp
+++ tank/boss.cpp
@@ -1,8 +1,8 @@
 #include "boss.h"
 
-BOSS::BOSS(int x,int y)
+BOSS::BOSS(int x, int y)
 {
-    this->setPixmap(QPixmap("://90Tank/enemy/space_full_preview.png"));
+    this->setPixmap(QPixmap("://90Tank/enemy/space_full_preview.png"));  // 设置BOSS的图像
 
-    this->setPos(x,y);
+    this->setPos(x, y);  // 设置BOSS的位置
 }
