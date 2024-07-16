b15859451fd867d24669fbf0f89cc94ae474d9ae
hzx150 <2167753545@qq.com>
2024-07-16 19:22:58 +0800

    Update enemy.h

--- tank/enemy.h
+++ tank/enemy.h
@@ -1,15 +1,18 @@
 #ifndef ENEMY_H
 #define ENEMY_H
+
 #include <QGraphicsPixmapItem>
 #include <QGraphicsItem>
-#include<QObject>
+#include <QObject>
 
-class Enemy : public QGraphicsPixmapItem,public QObject
+class Enemy : public QGraphicsPixmapItem, public QObject
 {
 public:
-    Enemy(int x,int y,int Ehp);
-    int enemySpeed=10;
-    int EHP;
+Enemy(int x，int y,int Ehp);  // 敌人类的构造函数
+int enemyspeed=10;//敌人的速度
+int EHP;//敌人血量
+
+private:
 };
 
 #endif // ENEMY_H
