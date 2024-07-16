00f20eaca97d6abbd57267ac5b678230610b40f1
hzx150 <2167753545@qq.com>
2024-07-16 19:21:14 +0800

    Update building.h

--- tank/building.h
+++ tank/building.h
@@ -4,11 +4,12 @@
 #include <QGraphicsItem>
 #include<QObject>
 
+// 定义一个Building类，继承自QGraphicsPixmapItem和QObject，用于表示游戏中的建筑物
 class Building : public QGraphicsPixmapItem,public QObject
 {
 public:
-    int kind;
-    Building(int x,int y,int kind);
+int kind;
+Building(int x,int y,int kind);
 };
 
 #endif // BUILDING_H
