080ff871f5da2eed6cee1daf916d3bab01dce1d1
hzx150 <2167753545@qq.com>
2024-07-16 19:19:36 +0800

    Update boss.h

--- tank/boss.h
+++ tank/boss.h
@@ -1,15 +1,16 @@
 #ifndef BOSS_H
 #define BOSS_H
+
 #include <QGraphicsPixmapItem>
 #include <QGraphicsItem>
-#include<QObject>
+#include <QObject>  // 包含QObject类的头文件，以便继承
 
-class BOSS : public QGraphicsPixmapItem,public QObject
+class BOSS : public QGraphicsPixmapItem, public QObject
 {
 public:
-    BOSS(int x,int y);
-    int bossSpeed=10;
-    int BossHp=50;
+    BOSS(int x, int y);  // 构造函数声明，用于初始化BOSS对象
+    int bossSpeed = 10;  // BOSS的移动速度
+    int BossHp = 50;  // BOSS的血量
 };
 
 #endif // BOSS_H
